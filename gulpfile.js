723f1b35af81e474ca51a49fd0be63837bfd4334
shofukuhara <sho.fukuhara0213@gmail.com>
2022-08-13 23:43:39 +0900

    study:2022.08.13.23:44

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,39 @@
 const { src, dest } = require("gulp");
-const rename = require("gulp-rename");
+const loadPlugins = require("gulp-load-plugins");
+const $ = loadPlugins();
+const sizes = [
+  [16, 16],
+  [32, 32],
+  [48, 48],
+  [57, 57],
+  [76, 76],
+  [120, 120],
+  [128, 128],
+  [152, 152],
+  [167, 167],
+  [180, 180],
+  [192, 192],
+  [512, 512],
+];
 
-function copyFiles() {
-  return src("./src/**/*.html")
-    .pipe(rename({prefix: "hello-"}))
-    .pipe(dest("./dist"));
+function item(done) {
+  for (let size of sizes) {
+    let width = size[0];
+    let height = size[1];
+    src("./item.png")
+      .pipe(
+        $.imageResize({
+          width,
+          height,
+          crop: true,
+          upscale: false,
+        })
+      )
+      .pipe($.imagemin())
+      .pipe($.rename(`item-${width}×${height}.png`))
+      .pipe(dest("./dist/images/item"));
+  }
+  done();
 }
 
-exports.copyFiles = copyFiles;
+exports.item = item;
